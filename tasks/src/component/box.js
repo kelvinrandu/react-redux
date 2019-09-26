98bcf29100bf9a063e07ef51b8c7e1607c414207
kelvinrandu <randukelvin@gmail.com>
2019-09-26 14:44:46 +0300

    added axios and redux to project

---
+++ tasks/src/component/box.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+class Box extends React.Component{
+    render(){
+        return(
+            <div> 
+                <button>click me </button>
+            </div>
+        )
+    }
+}
+
+export default Box;
--- tasks/src/index.js
+++ tasks/src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import BoxCon from './container/box-container.js';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
+
+
+class App extends React.Component{
+    render(){
+        return(
+            <BoxCon> </BoxCon>
+        )
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
