169037f6b9bf65b4d98791563d06b9a51b48903b
kelvinrandu <randukelvin@gmail.com>
2019-10-02 16:42:18 +0300

    completed login functionality

--- tasks/src/actions/user.actions.js
+++ tasks/src/actions/user.actions.js
@@ -1,20 +1,35 @@
-import axios from 'axios';
 import { userService } from '../services';
+import { history } from '../helpers';
+import { alertActions } from './';
 
 export const userActions = {
-    login,
+    login
 
 };
 
-function login(phone, password) {
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ phone, password })
-    };
-    return dispatch => {
-        return axios.post('https://kazi.azurewebsites.net/personnel/login',requestOptions);
-    }
 
+function login(phone, password) {
+    return dispatch => {
+
+        userService.post(phone, password)
+            .then(
+                
+                user => { 
+                    // dispatch(success(user));
+                    
+                    history.push('/');
+                },
+                error => {
+                    // dispatch(failure(error.toString()));
+                    dispatch(alertActions.error(error.toString()));
+                }
+            );
+    };
+
+    // function request(user) { return { type: userConstants.LOGIN_REQUEST, user } }
+    // function success(user) { return { type: userConstants.LOGIN_SUCCESS, user } }
+    // function failure(error) { return { type: userConstants.LOGIN_FAILURE, error } }
 }
+
+
 
--- tasks/src/services/user.service.js
+++ tasks/src/services/user.service.js
@@ -1,37 +1,33 @@
 import axios from 'axios';
 import config from '../config/config';
-import { authHeader } from '../helpers';
+
+
 
 export const userService = {
-    login
+    post,
+    get,
 
 };
 
-function login(phone, password) {
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ phone, password })
-    };
-    return dispatch => {
-        return axios.post('https://kazi.azurewebsites.net/personnel/login',requestOptions);
+function get(apiEndpoint){
+    return axios.get(config.baseUrl+apiEndpoint).then((response)=>{
+       return response;
+    }).catch((err)=>{
+       console.log(err);
+    })
+}
+function post(phone, password){
+    let apiEndpoint = '/personnel/login';
+    let body = {
+        'phone': phone,
+        'password': password
     }
+    
+    return axios.post(config.baseUrl+apiEndpoint, body).then((response)=>{
+        console.log(response);
+       return response;
+    }).catch((err)=>{
+       console.log(err);
+    })
+}
 
-
-// function handleResponse(response) {
-//     return response.text().then(text => {
-//         const data = text && JSON.parse(text);
-//         if (!response.ok) {
-//             if (response.status === 401) {
-//                 // auto logout if 401 response returned from api
-              
-//                 console.log('logged in')
-//             }
-
-//             const error = (data && data.message) || response.statusText;
-//             return Promise.reject(error);
-//         }
-
-//         return data;
-//     });
- }
