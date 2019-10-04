6a47e663678995a15c08266195e585732ec8b586
kelvinrandu <randukelvin@gmail.com>
2019-10-04 20:42:50 +0300

    added list task functionality in home page

--- tasks/src/components/home/home.component.js
+++ tasks/src/components/home/home.component.js
@@ -3,46 +3,50 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import  { GetAccessToken } from '../../helpers/auth-header';
 import { userActions } from '../../actions';
+import config from '../../config/config';
+import axios from 'axios'
 class Home extends Component {
-    constructor(props) {
-        super(props);
+state = {
+    tasks: []
+  }
+  componentDidMount(){
+    const accessToken = GetAccessToken();
 
-        this.state = {
-            tasks: []
-
-            
-        };
-    
-    }
-
-    componentDidMount() {
-        const accessToken = GetAccessToken();
-        if(accessToken){
-            const listtasks = userActions.getTasks().then((value) => { 
-                this.setState({ tasks: value });
-                 });;
-
-         
-        }else{
-            
-            this.props.history.push('/')
-
-        }
-
+    let apiEndpoint = '/tasks/assigned?page=1&limit=10&order=created&orderMethod=DESC';
+    axios.get(config.baseUrl+apiEndpoint, { headers: { Authorization : `Bearer ${accessToken}`} })
+      .then(res => {
+        console.log(res.data.tasks);
+        this.setState({
+          tasks: res.data.tasks
+        });
+      })
+  }
+  render(){
+    const { tasks } = this.state
+    const taskList = tasks.length ? (
+      tasks.map(task => {
+        return (
+          <div className="post card" key={task.task_id}>
+            <div className="card-content">
    
-    }
-     render() {
-         const { classes } = this.props;
-         console.log(this.state.tasks);
-         return (
-             <div className>
-                <h1>Home</h1>
-                <ul>
-       
-      </ul>
-             </div>
-         );
-     }
+              <p>{task.customer_first_name + '  ' + task.customer_last_name}</p>
+            </div>
+          </div>
+        )
+      })
+    ) : (
+      <div className="center">No tasks to show</div>
+    );
+
+    return (
+      <div>
+        <div className="container">
+          <h4 className="center">Tasks</h4>
+          {taskList}
+        </div>
+      </div>
+    )
+  }
 }
 Home.propTypes = {
     classes: PropTypes.object.isRequired
